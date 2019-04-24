4224085f79d600c054800a65c4e5460d2aa83125
Shawn Ren <shawnren527@gmail.com>
2019-04-24 03:01:24 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,14 @@ class ApplicationController < Sinatra::Base
     erb :show
   end
 
+  get '/articles/:id/edit' do
+    erb :edit
+  end
+
+  patch 'article/:id' do |id|
+    @article = Article.find(id)
+  end
+
 
 
 
